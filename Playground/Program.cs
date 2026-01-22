52b1e0fbeb6f9d50844923ab43718bf1244f3eed
Obiasoyiee <Obiasoyiee@HQ-TECH-L1495>
2026-01-22 11:28:57 +0100

    Add Code

---
+++ Playground/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(GetGlCodeFromLedger("123456789099999999996767"));
+
+
+ string GetGlCodeFromLedger(string ledger)
+{
+    var cleanLedger = ledger.Trim();
+
+
+    if (cleanLedger.Length == 0)
+    {
+        return string.Empty;
+    }
+
+    if (cleanLedger.Length > 12)
+    {
+        return cleanLedger.Substring(6, 6);
+    }
+
+    return string.Empty;
+}
